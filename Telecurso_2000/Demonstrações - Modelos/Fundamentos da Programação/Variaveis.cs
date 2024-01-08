ef4cf283796d122b480ca90accd201a08e74f6ac
Luigi Campregher França <111401072+Lubrisa@users.noreply.github.com>
2024-01-08 06:47:40 +0000

    ✍️ classe Variaveis
    Refatoração da classe Variaveis para mudar a abordagem das demonstrações.
    * Exclusão dos métodos DrawAddressTableEntry, DrawTypeTableEntry, WasInitialized, GetVariableDeclaration e GetIsOfTypeMessage.
    * Edição do método StringifyAddress.
    * Adição da classe aninhada VariaveisDrawer e do struct aninhado Variavel.

--- Telecurso_2000/Demonstrações - Modelos/Fundamentos da Programação/Variaveis.cs
+++ Telecurso_2000/Demonstrações - Modelos/Fundamentos da Programação/Variaveis.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using Telecurso2000Programacao.Ferramentas;
 
 namespace Telecurso2000Programacao.Demonstracoes.Modelos.FundamentosDaProgramacao
@@ -17,12 +19,21 @@ namespace Telecurso2000Programacao.Demonstracoes.Modelos.FundamentosDaProgramaca
             SeparatorDrawer.DrawSeparator("Definição de variável");
             Console.WriteLine();
 
-            int x = 0;
-            DrawAddressTableEntry(&x, nameof(x));
-            string y = "str";
-            DrawAddressTableEntry(&y, nameof(y));
-            bool z = false;
-            DrawAddressTableEntry(&z, nameof(z));
+            object inteiro = 0;
+            object real = 6.9f;
+            object caractere = '!';
+            object texto = "str";
+            object confirmacao = true;
+
+            Console.WriteLine(
+                TypeDataTableDrawer.DrawTable([
+                new Variavel<object>(nameof(inteiro), inteiro, &inteiro),
+                new Variavel<object>(nameof(real), real, &real),
+                new Variavel<object>(nameof(caractere), caractere, &caractere),
+                new Variavel<object>(nameof(texto), texto, &texto),
+                new Variavel<object>(nameof(confirmacao), confirmacao, &confirmacao)
+                ])
+            );
 
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator();
@@ -36,22 +47,6 @@ namespace Telecurso2000Programacao.Demonstracoes.Modelos.FundamentosDaProgramaca
             SeparatorDrawer.DrawSeparator("Tipos Primitivos");
             Console.WriteLine();
 
-            DrawTypeTableEntry(values: [true, false], notes: "internamente é um bit");
-            DrawTypeTableEntry(values: [byte.MinValue, byte.MaxValue]);
-            DrawTypeTableEntry(values: [sbyte.MinValue, sbyte.MaxValue]);
-            DrawTypeTableEntry(values: ['0', 'a', 'Z', '!'], notes: "inclui números, minúsculas, maiúsculas e sinais");
-            DrawTypeTableEntry(values: [decimal.MinValue, decimal.MaxValue], notes: "inclui decimais");
-            DrawTypeTableEntry(values: [double.MinValue, double.MaxValue], notes: "inclui decimais");
-            DrawTypeTableEntry(values: [float.MinValue, float.MaxValue], notes: "inclui decimais");
-            DrawTypeTableEntry(values: [int.MinValue, int.MaxValue]);
-            DrawTypeTableEntry(values: [uint.MinValue, uint.MaxValue]);
-            DrawTypeTableEntry(values: [nint.MinValue, nint.MaxValue], notes: "tamanho varia entre 32 e 64 bits");
-            DrawTypeTableEntry(values: [nuint.MinValue, nuint.MaxValue], notes: "tamanho varia entre 32 e 64 bits");
-            DrawTypeTableEntry(values: [long.MinValue, long.MaxValue]);
-            DrawTypeTableEntry(values: [ulong.MinValue, ulong.MaxValue]);
-            DrawTypeTableEntry(values: [short.MinValue, short.MaxValue]);
-            DrawTypeTableEntry(values: [ushort.MinValue, ushort.MaxValue]);
-
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator();
         }
@@ -63,16 +58,6 @@ namespace Telecurso2000Programacao.Demonstracoes.Modelos.FundamentosDaProgramaca
         {
             SeparatorDrawer.DrawSeparator("Declaração e Atribuição");
             Console.WriteLine();
-
-            int x;
-            string y = "declarado";
-            bool z = false;
-            object w;
-
-            Console.WriteLine(GetVariableDeclaration(&x, nameof(x)));
-            Console.WriteLine(GetVariableDeclaration(&y, nameof(y)));
-            Console.WriteLine(GetVariableDeclaration(&z, nameof(z)));
-            Console.WriteLine(GetVariableDeclaration(&w, nameof(w)));
 
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator();
@@ -88,31 +73,9 @@ namespace Telecurso2000Programacao.Demonstracoes.Modelos.FundamentosDaProgramaca
             SeparatorDrawer.DrawSeparator("Conversão Implícita");
             Console.WriteLine();
 
-            char a = 'a';
-            int b = a;
-            long c = b;
-            float d = c;
-            double e = d;
-            Console.WriteLine(GetIsOfTypeMessage(a));
-            Console.WriteLine(GetIsOfTypeMessage(b));
-            Console.WriteLine(GetIsOfTypeMessage(c));
-            Console.WriteLine(GetIsOfTypeMessage(d));
-            Console.WriteLine(GetIsOfTypeMessage(e));
-
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator("Conversão Explicita");
             Console.WriteLine();
-
-            double v = 173233333252.763363337d;
-            float w = (float)v;
-            long x = (long)w;
-            int y = (int)x;
-            char z = (char)y;
-            Console.WriteLine(GetIsOfTypeMessage(v));
-            Console.WriteLine(GetIsOfTypeMessage(w));
-            Console.WriteLine(GetIsOfTypeMessage(x));
-            Console.WriteLine(GetIsOfTypeMessage(y));
-            Console.WriteLine(GetIsOfTypeMessage(z));
 
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator();
@@ -128,144 +91,174 @@ namespace Telecurso2000Programacao.Demonstracoes.Modelos.FundamentosDaProgramaca
             SeparatorDrawer.DrawSeparator("Atribuição por Valor");
             Console.WriteLine();
 
-            int w = 0;
-            int x = w;
-            DrawAddressTableEntry(&w, nameof(w));
-            DrawAddressTableEntry(&x, nameof(x));
-
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator("Atribuição por Referência - Endereço da Variável");
             Console.WriteLine();
-
-            object y = new();
-            object z = y;
-            DrawAddressTableEntry(&y, nameof(y));
-            DrawAddressTableEntry(&z, nameof(z));
 
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator("Atribuição por Referência - Endereço do Valor");
             Console.WriteLine();
 
-            DrawAddressTableEntry(y, nameof(y));
-            DrawAddressTableEntry(z, nameof(z));
-
             Console.WriteLine();
             SeparatorDrawer.DrawSeparator();
         }
 
-        #region Helpers
         /// <summary>
-        /// Converte a referência de ponteiro em um endereço de memória.
+        /// Helper class that make strings that the <c>Variaveis</c> class needs.
         /// </summary>
-        /// <typeparam name="T">Tipo da variável referenciada pelo ponteiro.</typeparam>
-        /// <param name="pointer">O ponteiro que terá sua referência convertida.</param>
-        /// <returns>Um endereço de memória em string.</returns>
-        private static string StringfyAddress<T>(T* pointer)
+        private static class VariaveisDrawer
         {
-            string addressToHexadecimal = ((nint)pointer).ToString("X");
-            return $"0x{addressToHexadecimal}";
+            /// <summary>
+            /// Stringifies the address pointed by a pointer.
+            /// </summary>
+            /// <param name="pointer">The pointer that holds the address that will be stringified.</param>
+            /// <returns>A string with a memory address.</returns>
+            public static string StringifyAddress<T>(T* pointer)
+            {
+                string ADDRESS_PREFIX = "0x";
+                nint Address = (nint)pointer;
+
+                return ADDRESS_PREFIX + Address.ToString("X");
+            }
+
+            /// <summary>
+            /// Builds a string representing a entry in a table that contains a variable's name, memory address and value.
+            /// </summary>
+            /// <param name="variable">The variable that will be used to build the table entry.</param>
+            /// <returns>A string formatted like a table entry containing the info about the given variable.</returns>
+            public static string GetAddressTableEntry<T>(Variavel<T> variable)
+            {
+                const string NAME_LABEL = $"Nome";
+                const string ADDRESS_LABEL = $"Endereço";
+                const string VALUE_LABEL = $"Valor";
+                const string NULL_VALUE_LABEL = "NULL";
+
+                int tableColumSize = GetLongestStringSize(NAME_LABEL,
+                    variable.Nome,
+                    ADDRESS_LABEL,
+                    variable.Endereco,
+                    VALUE_LABEL,
+                    variable.Valor?.ToString() ?? NULL_VALUE_LABEL);
+
+
+                string entryHeader = BuildTableEntryLine(tableColumSize, NAME_LABEL, ADDRESS_LABEL, VALUE_LABEL);
+                string entryBody = BuildTableEntryLine(tableColumSize,
+                    variable.Nome,
+                    variable.Endereco,
+                    variable.Valor?.ToString() ?? NULL_VALUE_LABEL);
+
+                return BuildTableEntry(entryHeader, entryBody);
+            }
+
+            /// <summary>
+            /// Builds a string representing a entry in a table that contains a primitive type name, and default value.
+            /// </summary>
+            /// <param name="T">The primitive type that will be have it's info listed in the table.</param>
+            /// <returns>A string formatted like a table entry containing the info about the given primitive.</returns>
+            public static string GetPrimitiveInfoTableEntry<T>() where T : unmanaged
+            {
+                const string NAME_LABEL = "Nome";
+                const string DEFAULT_LABEL = "Valor Padrão";
+
+                string typeName = typeof(T).Name;
+                string typeDefaultValue = typeof(T) == typeof(char) ? "\"\"" : default(T).ToString()!;
+
+                int tableColumSize = GetLongestStringSize(NAME_LABEL,
+                    typeName,
+                    DEFAULT_LABEL,
+                    typeDefaultValue);
+
+                string entryHeader = BuildTableEntryLine(tableColumSize, NAME_LABEL, DEFAULT_LABEL);
+                string entryBody = BuildTableEntryLine(tableColumSize, typeName, typeDefaultValue);
+
+                return BuildTableEntry(entryHeader, entryBody);
+            }
+
+            /// <summary>
+            /// Find and return the size of the longest string between the given strings.
+            /// </summary>
+            /// <param name="strings">The strings that will have their lengths compared.</param>
+            /// <returns>The length of the longest string. 0 if the given array is empty.</returns>
+            /// <exception cref="ArgumentException"></exception>
+            private static int GetLongestStringSize(params string[] strings)
+            {
+                if (strings is null)
+                    throw new ArgumentException("The argument is null");
+
+                if (strings.Length == 0)
+                    return 0;
+
+                return strings
+                    .MaxBy(texto => texto?.Length ?? 0)?
+                    .Length ?? 0;
+            }
+
+            /// <summary>
+            /// Builds a line of a table entry body with the columns values.
+            /// </summary>
+            /// <param name="columnsSize">The size of each column.</param>
+            /// <param name="columnsValues">The text contained in each column.</param>
+            /// <returns>A string with the table entry body.</returns>
+            /// <exception cref="ArgumentException"></exception>
+            private static string BuildTableEntryLine(int columnsSize, params string[] columnsValues)
+            {
+                if (columnsValues is null)
+                    throw new ArgumentException("The \"columnsValues\" argument is null");
+
+                const char SEPARATOR_CHAR = '|';
+
+                var sb = new StringBuilder(SEPARATOR_CHAR.ToString());
+                foreach (var valueString in from columnValue in columnsValues
+                                            select columnValue?.ToString())
+                {
+                    sb.Append(valueString)
+                        .Append(string.Empty.PadRight(columnsSize - valueString.Length))
+                        .Append(SEPARATOR_CHAR);
+                }
+
+                return sb.ToString();
+            }
+
+            /// <summary>
+            /// Builds the entry of a table with the given entry body (content).
+            /// </summary>
+            /// <param name="entryBody"></param>
+            /// <returns></returns>
+            private static string BuildTableEntry(params string[] entryBody)
+            {
+                int tableLength = GetLongestStringSize(entryBody);
+
+                string tableSeparator = string.Empty.PadRight(tableLength, '-');
+
+                var sb = new StringBuilder();
+                foreach (var entryLayer in entryBody)
+                    sb.AppendLine(tableSeparator)
+                        .AppendLine(entryLayer);
+                sb.Append(tableSeparator);
+
+                return sb.ToString();
+            }
         }
 
         /// <summary>
-        /// Converte a referência de uma instância de um tipo de referência em um endereço de memória.
+        /// Uma estrutura para representar uma variável.
         /// </summary>
-        /// <typeparam name="T">O tipo da referência.</typeparam>
-        /// <param name="obj">O objeto que terá seu endereço retornado.</param>
-        /// <returns>Um endereço de memória em string.</returns>
-        private static string StringfyAddress<T>(T obj) where T : class
+        /// <typeparam name="T">O tipo da variável.</typeparam>
+        /// <param name="Nome">O nome da variável.</param>
+        /// <param name="Valor">O valor da variável.</param>
+        private readonly record struct Variavel<T>(string Nome, T Valor)
         {
-            var handle = GCHandle.Alloc(obj, GCHandleType.Pinned);
-            string handleToHexadecimal = handle.AddrOfPinnedObject().ToString("X");
-            handle.Free();
+            /// <summary>
+            /// O endereço de memória da variável.
+            /// </summary>
+            public readonly string Endereco { get; init; }
 
-            return $"0x{handleToHexadecimal}";
+            /// <param name="nome">O nome da variável.</param>
+            /// <param name="valor">O valor da variável.</param>
+            /// <param name="endereco">O endereço de memória da variável.</param>
+            public Variavel(string nome, T valor, T* endereco) : this(nome, valor) =>
+                Endereco = VariaveisDrawer.StringifyAddress(endereco);
         }
-
-        /// <summary>
-        /// Escreve no console o nome, valor e endereço de uma variável em uma estrutura de tabela.
-        /// </summary>
-        /// <typeparam name="T">O tipo da variável referenciada pelo ponteiro.</typeparam>
-        /// <param name="pointer">Um ponteiro com uma referência a um endereço de memória.</param>
-        /// <param name="variableName">O nome da variável referenciada pelo ponteiro.</param>
-        private static void DrawAddressTableEntry<T>(T* pointer, string variableName)
-        {
-            string variableColum = $"Variável: {variableName}".PadRight(25);
-            string valueColum = $"Valor: {*pointer}".PadRight(25);
-            string addressColum = $"Endereço: {StringfyAddress(pointer)}";
-
-            Console.WriteLine(variableColum + valueColum + addressColum);
-        }
-
-        private static void DrawAddressTableEntry<T>(T obj, string variableName) where T : class
-        {
-            string variableColum = $"Variável: {variableName}".PadRight(25);
-            string valueColum = $"Valor: {obj}".PadRight(25);
-            string addressColum = $"Endereço: {StringfyAddress(obj)}";
-
-            Console.WriteLine(variableColum + valueColum + addressColum);
-        }
-
-        /// <summary>
-        /// Escreve no console o nome, valores importantes e algumas notas sobre um tipo.
-        /// </summary>
-        /// <typeparam name="T">O tipo que será descrito.</typeparam>
-        /// <param name="notes">Notas sobre o tipo descrito.</param>
-        /// <param name="values">Valores importantes do tipo descrito.</param>
-        private static void DrawTypeTableEntry<T>(string notes = "", params T[] values) where T : struct
-        {
-            string typeColum = $"Tipo: {typeof(T).Name}".PadRight(25);
-
-            string valuesList = values
-                .Select(value => value.ToString())
-                .Aggregate("", (accumulator, value) => accumulator + $"{value}, ")[..^2];
-            string valuesColum = $@"Valores: {valuesList}"
-                .PadRight(75);
-
-            string notesColum = $"Notas: {notes}";
-
-            Console.WriteLine(typeColum + valuesColum + notesColum);
-        }
-
-        /// <summary>
-        /// Verifica se uma variável foi inicializada.
-        /// </summary>
-        /// <typeparam name="T">O tipo da variável que será verificada.</typeparam>
-        /// <param name="pointer">Um ponteiro com o endereço da variável que será verificada.</param>
-        /// <returns>True se a variável foi inicializada, false caso ela provavelmente não tenha sido.</returns>
-        private static bool WasInitialized<T>(T* pointer) => !Equals(*pointer, default(T));
-
-        /// <summary>
-        /// Cria uma mensagem que corresponde a como uma variável foi declarada de acordo com seu estado atual.
-        /// </summary>
-        /// <typeparam name="T">O tipo da variável que será escrita.</typeparam>
-        /// <param name="pointer">Um ponteiro para o endereço da variável que será escrita.</param>
-        /// <param name="variableName">O nome da variável escrita.</param>
-        /// <returns>Uma mensagem que corresponde a como a variável deve ter sido declarada de acordo com seu valor atual.</returns>
-        private static string GetVariableDeclaration<T>(T* pointer, string variableName)
-        {
-            const string UNINITIALIZED_MESSAGE = "// A variável provavelmente não foi inicializada";
-            const string INITIALIZED_MESSAGE = "// A variável foi inicializada";
-
-            return $"{typeof(T)} {variableName} "
-                + (WasInitialized(pointer)
-                    ? $"= {*pointer} " + INITIALIZED_MESSAGE
-                    : UNINITIALIZED_MESSAGE)
-                + ";";
-        }
-
-        /// <summary>
-        /// Cria uma mensagem que diz qual o tipo de um valor.
-        /// </summary>
-        /// <typeparam name="T">O tipo do valor que será descrito na mensagem.</typeparam>
-        /// <param name="value">O valor que terá seu tipo descrito.</param>
-        /// <returns>Uma mensagem dizendo qual é o tipo do valor passado.</returns>
-        private static string GetIsOfTypeMessage<T>(T value)
-        {
-            const string IS_OF_TYPE_MESSAGE = "é um valor do tipo: ";
-
-            return $"{value} {IS_OF_TYPE_MESSAGE} {typeof(T)}";
-        }
-        #endregion
 #pragma warning restore CS8500 // Isso pega o endereço, obtém o tamanho ou declara um ponteiro para um tipo gerenciado
     }
 }
